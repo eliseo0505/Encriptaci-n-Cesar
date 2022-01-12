4a3c1237d8f0464c7f18ddc3e0e6ebbc4348bdf8
Eliseo Esteverena <eliseo050595@gmail.com>
2022-01-12 11:25:52 -0300

    Funcionalidad de Descifrado añadida

--- main.go
+++ main.go
@@ -4,40 +4,69 @@ import (
 	"fmt"
 )
 
-var letras = []rune("ABCDEÉGHIÍJKLMNÑOÓPQRSTUÚVWXYZabcdeéfghiíjklmnñoópqrstuúvwxyz1234567890 .:,;¡!¿?''+-*/-=_{}[]()&%$#\\\"<>|°") // 105 Caracteres
+var letras = []rune("ABCDEÉGHIÍJKLMNÑOÓPQRSTUÚVWXYZaábcdeéfghiíjklmnñoópqrstuúvwxyz1234567890 .:,;¡!¿?'/+-*=_{}[]()&%$#\\\"<>|@°€¥£") // 108 Caracteres
 
-var texto = "A" //Í
+var texto = "Encriptación Cesár - Ejercicio de programación en Go."
 
-var displaceGlobal = 10 // Desplazamiento
+var displaceGlobal = 20 // Desplazamiento
 
 func main() {
 	fmt.Println("Desplazamiento: ", displaceGlobal)
-	fmt.Println("Texto: ", texto)
+	fmt.Print("Texto: ", texto)
 	fmt.Println(" ")
 
 	cifrarTexto()
+
 }
 
 func cifrarTexto() string {
-	fmt.Println("Cifrado: ")
 	cifrado := ""
-	x := string(letras[67:105])
-	runeDos := []rune(x + string(letras[0:67]))
+	runeDos := []rune("7890 .:,;¡!¿?'/+-*=_{}[]()&%$#\"<>|@°€¥£ABCDEÉGHIÍJKLMNÑOÓPQRSTUÚVWXYZaábcdeéfghiíjklmnñoópqrstuúvwxyz123456")
 	for _, e := range texto {
 		for k, v := range letras {
 			displace := k + displaceGlobal
-			if e == v && displace <= 104 {
+			if e == v && displace <= 107 {
 				cifrado = cifrado + string(letras[displace])
 			}
-			if e == v && displace > 104 {
+			if e == v && displace > 107 {
 				for krD, vrD := range runeDos {
 					if vrD == e {
-						cifrado = cifrado + string(runeDos[krD+displaceGlobal])
+						sumarPosiciones := krD + displaceGlobal
+						cifrado = cifrado + string(runeDos[sumarPosiciones])
 					}
 				}
 			}
 		}
 	}
-	fmt.Println(cifrado)
+	fmt.Println("Cifrado: ", cifrado)
+
+	descifrar(cifrado)
+
 	return cifrado
+}
+
+func descifrar(cifrado string) {
+	descifrado := ""
+	x := []rune("RSTUÚVWXYZaábcdeéfghiíjklmnñoópqrstuúvwxyz1234567890 .:,;¡!¿?'/+-*=_{}[]()&%$#\"<>|@°€¥£ABCDEÉGHIÍJKLMNÑOÓPQ")
+	for _, v := range cifrado {
+		for k, vl := range letras {
+			displace := k - displaceGlobal
+			if v == vl && k >= displaceGlobal {
+				descifrado += string(letras[displace])
+			}
+			if vl == v && k < displaceGlobal {
+				for kX, vX := range x {
+					if vX == v && kX >= displaceGlobal {
+						restarPosiciones := kX - displaceGlobal
+						descifrado += string(x[restarPosiciones])
+					}
+					if vX == v && kX <= displaceGlobal {
+						restarPosiciones := displaceGlobal - kX
+						descifrado += string(x[restarPosiciones])
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("Descifrado: ", descifrado)
 }
